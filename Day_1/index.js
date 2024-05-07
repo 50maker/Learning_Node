0f8a63c86553b2846b41a22233555f1bf4aaa210
Milan <milankc5000@gmail.com>
2024-05-07 18:42:19 +0545

    Complete Day_1

---
+++ Day_1/index.js
@@ -0,0 +1,10 @@
+console.log("Hello World!");
+
+// Let and const only works on scope level and var works on global level.
+
+const name = "Texas";
+
+// Only global truth value are assigned capital naming
+// we cannot break higher order functions like map and forEach
+// === checks both value and data types
+// complex data object uses memory references to assign values than primitive data which uses only assigned values
